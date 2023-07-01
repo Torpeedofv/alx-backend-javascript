e525692877a6e6b9b498d286ac043c4e6fe2c8fe
Torpeedofv <torpeedo.fv@gmail.com>
2023-07-01 18:15:36 +0100

    task 2 correction

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      console.log('Got a response from the API');
+      
       return { status: 200, body: 'Success' };
     })
-    .catch(() => {
-      console.log('Got a response from the API');
-      throw new Error('The fake API is not working currently');
-    });
+    .catch(() => new Error())
+	.finally(() => console.log('Got a response from the API'))
 }
