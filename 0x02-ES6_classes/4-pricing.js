d3544d6f6ee75c9ff7c775fa5c3fd6694f5fdfea
Torpeedofv <torpeedo.fv@gmail.com>
2023-07-02 22:00:14 +0100

    task 4 correction

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -23,7 +23,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this._amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
