1add74783b8ef33e4ade7883ab7a41dfcdcc021b
Torpeedofv <torpeedo.fv@gmail.com>
2023-07-03 20:00:29 +0100

    Readme

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
-export default function getListStudents () {
-	const object1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' }
-	const object2 = { id: 2, firstName: 'James', location: 'Columbia' }
-	const object3 = { id: 5, firstName: 'Serena', location: 'San Francisco' }
-	return [object1, object2, object3]
+export default function getListStudents() {
+  const object1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+  const object2 = { id: 2, firstName: 'James', location: 'Columbia' };
+  const object3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  return [object1, object2, object3];
 }
