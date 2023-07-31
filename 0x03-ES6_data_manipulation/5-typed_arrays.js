1ade3ac427f5f1ecb8fbe509c602395dba94ffeb
Torpeedofv <torpeedo.fv@gmail.com>
2023-07-31 23:30:30 +0100

    task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,2 +1,10 @@
-export default function createInt8TypedArray(length=0, position=0, value=0) {
+export default function createInt8TypedArray(length = 0, position = 0, value = 0) {
+  const buffer = new ArrayBuffer(length);
+  const view = new DataView(buffer, 0, length);
 
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  view.setInt8(position, value);
+  return view;
+}
