3f592852b54c3a09662567da0b9afc8ae7173592
Torpeedofv <torpeedo.fv@gmail.com>
2023-08-01 15:20:05 +0100

    task 8 redo

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   const endString = [];
   if (startString === '' || typeof startString !== 'string') return '';
   for (const value of set) {
-    if (value.startsWith(startString)) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
       endString.push(value.slice(startString.length));
     }
   }
