7ac408af6c58c24130f63ffad701a0fa3bdf03c2
Torpeedofv <torpeedo.fv@gmail.com>
2023-08-01 15:40:42 +0100

    task 9

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,3 +1,16 @@
 export default function groceriesList() {
-	const map = new Map();
+  const map = new Map();
+  const groceries = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
 
+  for (const [key, value] of groceries) {
+    map.set(key, value);
+  }
+
+  return map;
+}
