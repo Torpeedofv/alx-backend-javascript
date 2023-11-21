bc07a198a594c6321ab31f6e1f7ee6f1dca8d599
Torpeedofv <torpeedo.fv@gmail.com>
2023-11-21 22:26:46 +0100

    task 1

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,19 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (name) => {
+  if (name) {
+    console.log(`Your name is: ${name}`);
+    rl.close();
+    if (!process.stdin.isTTY) {
+      console.log('This important software is now closing');
+      rl.close();
+    }
+  }
+});
