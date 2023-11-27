5a3a06f3c98d74800957edd559f19ec41b79e909
Torpeedofv <torpeedo.fv@gmail.com>
2023-11-27 14:38:52 +0000

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,31 +1,11 @@
-/*const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (name) => {
-  if (name) {
-    console.log(`Your name is: ${name}`);
-    rl.close();
-    if (!process.stdin.isTTY) {
-      console.log('This important software is now closing');
-    }
-  }
-});*/
-
-
 process.stdout.write('Welcome to Holberton School, what is your nameQ\n');
 
 process.stdin.on('readable', () => {
-	const name = process.stdin.read();
-	if (name) {
-		process.stdout.write(`Your name is : ${name}`);
-	}
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is : ${name}`);
+  }
 });
 process.stdin.on('close', () => {
-	process.stdout.write('This important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
